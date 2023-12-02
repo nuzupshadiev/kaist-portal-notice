e5124b3dc01d7fabfd1952e7f9f2358855d14e55
Nuzup Shadiev <nuzupshadiev@Nuzups-MacBook-Pro.local>
2023-12-03 02:37:47 +0900

    commit

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-const token = '6574074066:AAGvuyuTekYgnyPsI_1QjM9cRxiXWzn7CkI';
-const CHAT_ID = '778315062';
-const HTML_URL = 'https://portal.kaist.ac.kr/board/list.brd?boardId=International';
+import TelegramBot from 'node-telegram-bot-api';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+const token = '6574074066:AAFj5_XQsRiTMqLweKO8tQjXPETdRI2kI4s';
+const international_url = 'https://portal.kaist.ac.kr/board/list.brd?boardId=International';
+const student_notice_url = 'https://portal.kaist.ac.kr/board/list.brd?boardId=student_notice';
+const apiUrl = 'https://api.openai.com/v1/chat/completions';
+const openaiApiKey = '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'; // Replace with your 
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${openaiApiKey}`,
+};
 
 const bot = new TelegramBot(token, {polling: true});
 const previousPosts = [];
+
+const chatIds = [];
 
 function isDuplicate(obj1, obj2) {
   return (
@@ -16,6 +23,29 @@ function isDuplicate(obj1, obj2) {
     obj1.date === obj2.date
   );
 }
+
+// function handleTranslate(text) {
+//   return new Promise((resolve, reject) => {
+//     let requestData = {
+//       model: 'gpt-3.5-turbo',
+//       messages: [
+//         { role: 'system', content: 'You will be provided with a Korean text. Please translate it to English.' },
+//       ],
+//       temperature: 0,
+//       max_tokens: 256,
+//     };
+//     requestData.messages.push({ role: 'user', content: text });
+
+//     return axios.post(apiUrl, requestData, { headers })
+//     .then(response => {
+//       resolve(response.data.choices[0].message.content)
+//     })
+//     .catch(error => {
+//       console.error('Error:', error.response ? error.response.data : error.message);
+//       reject(error);
+//     });
+//   })
+// }
 
 function extractPosts(html) {
   const $ = cheerio.load(html);
@@ -34,21 +64,42 @@ function extractPosts(html) {
       });
   });
 
-  // Sort posts by the latest date
   posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return posts;
 }
 
-function sendNotification(message) {
-  bot.sendMessage(CHAT_ID, message, { parse_mode: 'Markdown' })
+function sendNotification(message, chatId, retryCount = 3) {
+  try {
+    bot.sendMessage(chatId, message, { parse_mode: "Markdown" })
+      .then((response) => {
+        console.log("Message sent");
+      })
+      .catch((error) => {
+        console.error(error);
+        if (retryCount > 0) {
+          console.log(`Retrying... (${retryCount} attempts left)`);
+          sendNotification(message, chatId, retryCount - 1);
+        } else {
+          console.error("Maximum retries reached. Notification not sent.");
+        }
+      });
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+  }
+}
+
+function SendNotificationEveryone(message){
+  for(const chatId of chatIds){
+    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' })
     .catch((error) => {
-      console.error(`Telegram API error: ${error.message}`);
+      console.error(`SendNotificationEveryone error: ${error.message}`);
     });
+  }
 }
 
 function checkForNewPosts() {
-  axios.get(HTML_URL).then((response)=>{
+  axios.get(international_url).then((response)=>{
     const html = response.data;
     const currentPosts = extractPosts(html);
     if (currentPosts.length > 0) {
@@ -57,15 +108,59 @@ function checkForNewPosts() {
         );
         if (newPosts.length > 0) {
           for (const post of newPosts) {
-            sendNotification(`New post:\n*${post.title}*\n(https://portal.kaist.ac.kr/${post.link})\n${post.date}`);
+            SendNotificationEveryone(`New post from International Community:\n*${post.title}*\n(https://portal.kaist.ac.kr${post.link})\n${post.date}`);
           }
           previousPosts.push(...newPosts);
         }
     }
   })
   .catch((err)=>console.error(err))
+
+  // axios.get(student_notice_url)
+  // .then((response)=>{
+  //   const html = response.data;
+  //   const currentPosts = extractPosts(html);
+  //   if (currentPosts.length > 0) {
+  //       const newPosts = currentPosts.filter(currentPost =>
+  //         !previousPosts.some(previousPost => isDuplicate(currentPost, previousPost))
+  //       );
+  //       if (newPosts.length > 0) {
+  //         for (const post of newPosts) {
+  //           SendNotificationEveryone(`New post from Student Notice:\n*${post.title}*\n(https://portal.kaist.ac.kr${post.link})\n${post.date}`);
+  //         }
+  //         previousPosts.push(...newPosts);
+  //       }
+  //   }
+  // })
+  // .catch((err)=>console.error(err))
 }
 
+function checkStartPosts(chatId) {
+  axios.get(international_url).then((response)=>{
+    const html = response.data;
+    const currentPosts = extractPosts(html);
+    if (currentPosts.length > 0) {
+        for(let i = 0; i < Math.min(5, currentPosts.length); i++){
+          const post = currentPosts[i]
+          sendNotification(`New post from International Community:\n*${post.title}*\n(https://portal.kaist.ac.kr${post.link})\n${post.date}`, chatId);
+        }
+    }
+  })
+  .catch((err)=>console.error(err))
+
+  // axios.get(student_notice_url).then((response)=>{
+  //   const html = response.data;
+  //   const currentPosts = extractPosts(html);
+  //   if (currentPosts.length > 0) {
+  //       for(let i = 0; i < Math.min(5, currentPosts.length); i++){
+  //         const post = currentPosts[i]
+  //         sendNotification(`New post from International Community:\n*${post.title}*\n(https://portal.kaist.ac.kr${post.link})\n${post.date}`, chatId);
+  //       }
+  //   }
+  // })
+  // .catch((err)=>console.error(err))
+
+}
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
 
@@ -80,8 +175,40 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  const user = msg.from;
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Мен сүйлөй албайм. Сизге билдирүү гана жөнөтө алам. Эгер кандайдыр бир көйгөй болсо, Нүзүпкө жазыңыз.');
+  if (!chatIds.includes(chatId)) {
+    chatIds.push(chatId);
+    let name = `${user.first_name} ${user.last_name}`;
+    const greetingMessage = `Hello ${name}! Welcome to KAIST International Portal Bot! You will be notified of any new posts from the portal. Thank you for joining our bot!`;
+    bot.sendMessage(chatId, greetingMessage)
+    .then(() => {})
+    .catch((error) => {
+      console.error(`sendNotification error: ${error.message}`);
+    })
+  }
+  if (msg.text && msg.text.toLowerCase() === '/start') {
+    checkStartPosts(chatId)
+  }
+  else if (msg.text && msg.text.toLowerCase() === '/help') {
+    bot.sendMessage(chatId, 'Please contact @everforgetmenot for any questions.')
+    .then(() => {console.log('help message sent')})
+    .catch((error) => {
+      console.error(`sendNotification error: ${error.message}`);
+    })
+  }
+  else if(msg.text === 'Салам' || msg.text === 'салам' || msg.text === 'Салам алейкум' || msg.text === 'салам алейкум'){
+    bot.sendMessage(chatId, 'Ваалейкум салам')
+    .then(() => {console.log('help message sent')})
+    .catch((error) => {
+      console.error(`sendNotification error: ${error.message}`);
+    })
+  }
+  else bot.sendMessage(chatId, 'If you have any questions, please contact @everforgetmenot')
+  .then(() => {console.log('help message sent')})
+  .catch((error) => {
+    console.error(`sendNotification error: ${error.message}`);
+  })
 });
 
 setInterval(() => {
